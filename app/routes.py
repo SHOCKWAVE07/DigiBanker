3ef76f0182f03a373c6de404d38bc0182925f756
shockwave07 <varunraskar22@gmail.com>
2024-03-04 01:06:34 +0530

    authentication started

---
+++ app/routes.py
@@ -0,0 +1,22 @@
+from app import app
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html',title='Home')
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash(f'Login request for user {form.username.data}, remember_me {form.remember_me.data}')
+        return redirect(url_for('index'))
+
+    return render_template('login.html', form=form, title='Login')
+
+
+
