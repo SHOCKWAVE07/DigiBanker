8e2f73a73405b387c7a4f5043d1691ec033722c4
shockwave07 <varunraskar22@gmail.com>
2024-03-04 01:36:04 +0530

    accounts table created

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from datetime import datetime, timezone
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
@@ -9,5 +10,22 @@ class User(db.Model):
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
 
+    accounts: so.WriteOnlyMapped['Account'] = so.relationship(back_populates='user')
+
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+class Account(db.Model):
+    account_no: so.Mapped[int] = so.mapped_column(primary_key=True)
+    balance: so.Mapped[int] = so.mapped_column()
+    city: so.Mapped[str] = so.mapped_column(sa.String(40))
+    mob_no: so.Mapped[int] = so.mapped_column()
+    timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
+    account_type : so.Mapped[str] = so.mapped_column(sa.String(40))
+    username: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.username), index=True)
+
+    user: so.Mapped[User] = so.relationship(back_populates='accounts')
+
+    def __repr__(self):
+        return '<Account {}>'.format(self.account_no)
--- run.py
+++ run.py
@@ -1,1 +1,8 @@
-from app import app
+from app import app, db
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app.models import User, Account
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Account': Account}
